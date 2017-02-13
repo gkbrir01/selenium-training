c6942ca286bd98fe52ca2953766cfae23e8d2d78
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-02-13 20:41:19 +0100

    1 wersja
    Dla przegladarki IE

---
+++ csharp-example/csharp-example/MyFirstTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            
+            driver = new InternetExplorerDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.pl";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Szukaj w Google"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+      }
+    }
