6addb1875dc95cf176fe37c1271e0037afb08972
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-02-17 17:42:51 +0100

    Lesson 2, work 3
    Lesson 2, work 3

---
+++ csharp-example/csharp-example/My2Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
+
+namespace csharp_example
+{
+    [TestFixture]
+    public class My2Test
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
