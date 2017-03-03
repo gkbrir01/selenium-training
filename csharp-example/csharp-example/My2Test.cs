a2bdf629097f7583dc3df018654fd69330de2505
Grzegorz Kozlowski <gkbrir01@gmail.com>
2017-03-03 22:08:22 +0100

    Work 7 and 8
    Work 7 and 8

--- csharp-example/csharp-example/My2Test.cs
+++ csharp-example/csharp-example/My2Test.cs
@@ -7,6 +7,8 @@ using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 using System.Threading;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace csharp_example
 {
@@ -15,24 +17,127 @@ namespace csharp_example
     {
         private IWebDriver driver;
         private WebDriverWait wait;
+        public const string nazwaPrzegladarki = "Chrome";
+        int i = 0;
 
         [SetUp]
         public void start()
         {
-            driver = new ChromeDriver();
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            if (nazwaPrzegladarki == "Firefox")
+            {
+                FirefoxOptions options = new FirefoxOptions();
+                driver = new FirefoxDriver();
+            }
+
+            if (nazwaPrzegladarki == "FirefoxESR")
+            {
+                FirefoxOptions options = new FirefoxOptions();
+                options.BrowserExecutableLocation = @"c:\Program Files\Mozilla FirefoxESR\firefox.exe";
+                driver = new FirefoxDriver(options);
+            }
+
+            if (nazwaPrzegladarki == "FirefoxN")
+            {
+                FirefoxOptions options = new FirefoxOptions();
+                options.BrowserExecutableLocation = @"c:\Program Files\Nightly\firefox.exe";
+                driver = new FirefoxDriver(options);
+            }
+
+
+            if (nazwaPrzegladarki == "IE")
+            {
+                driver = new InternetExplorerDriver();
+            }
+
+            if (nazwaPrzegladarki == "Chrome")
+            {
+                //ChromeOptions optionsChrome = new ChromeOptions();
+                //Console.WriteLine(optionsChrome);
+                driver = new ChromeDriver();
+            }
+
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
+            driver.Manage().Window.Maximize();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
         }
 
+
+        //WORK 7
         [Test]
-        public void SecondTest()
+        public void Test7()
         {
             driver.Navigate().GoToUrl("http://localhost/litecart/admin");
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
-            Thread.Sleep(2000);
             driver.FindElement(By.Name("login")).Click();
             wait.Until(ExpectedConditions.TitleIs("My Store"));
+
+            //Number of main menu items
+            int mEnd = driver.FindElements(By.CssSelector("#app-")).Count;
+           
+            for (int m = 1; m <= mEnd; m++)
+            {
+
+                IWebElement menu = driver.FindElement(By.CssSelector("#app-:nth-child(" + m + ")"));
+                menu.Click();
+
+                //The number of nested points in the main menu item                
+                int sEnd = driver.FindElements(By.CssSelector("#app-:nth-child(" + m + ") li")).Count;
+                                
+                for (int s = 1; s <= sEnd; s++)
+                {
+                    IWebElement submenu = driver.FindElement(By.CssSelector("#app-:nth-child(" + m + ") li:nth-child(" + s + ")"));
+                    String submenuTXT = submenu.Text;
+                    submenu.Click();
+
+                    //comparing the header with the menu item
+                    IWebElement title = driver.FindElement(By.CssSelector("h1"));
+                    String titleTXT = title.Text;
+                    if (submenuTXT != titleTXT)
+                    {
+                        Console.WriteLine("submenu "+ submenuTXT+" and title "+ titleTXT+" are not the same");
+                    }
+                }
+            }
+        }
+
+
+
+
+        //WORK 8
+        [Test]
+        public void Test8()
+        {
+            driver.Navigate().GoToUrl("http://localhost/litecart");
+            wait.Until(ExpectedConditions.TitleIs("Online Store | My Store"));
+            //Thread.Sleep(1000);
+
+            IList<IWebElement> elements = driver.FindElements(By.CssSelector("a.link[title$=Duck]"));
+            
+            foreach (IWebElement element in elements)
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Duck " + ++i);
+                Console.WriteLine(element.Location);
+                //Console.WriteLine(element.Text);
+                
+                IList<IWebElement> sticks = element.FindElements(By.CssSelector("div.sticker"));
+
+                if(sticks.Count > 1)
+                {
+                    Console.WriteLine("Duck has more than 1 stick !!!!!");
+                }
+
+                Console.WriteLine("The number od stickers: "+ sticks.Count);
+
+                foreach (IWebElement stick in sticks)
+                {
+                
+                    Console.WriteLine("Kind of sticker: " + stick.Text);
+                }
+                
+              }
         }
 
         [TearDown]
--- csharp-example/csharp-example/MyFirstTest.cs
+++ csharp-example/csharp-example/MyFirstTest.cs
@@ -5,37 +5,98 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
 
 namespace csharp_example
 {
     [TestFixture]
     public class MyFirstTest
     {
-        private IWebDriver driver;
+        //private IWebDriver driver;
         private WebDriverWait wait;
+        private IWebDriver chromeDriver;
+        private IWebDriver ieDriver;
+        private IWebDriver firefoxDriver;
+        public const string nazwaPrzegladarki = "Chrome";
 
         [SetUp]
         public void start()
         {
-            driver = new ChromeDriver();
-            //driver = new InternetExplorerDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            if (nazwaPrzegladarki == "Firefox")
+            {
+                //Firefox nowy
+                FirefoxOptions optionsFirefox = new FirefoxOptions();
+                optionsFirefox.UseLegacyImplementation = false;
+                //FirefoxProfile profile = new FirefoxProfile();
+                //profile.SetPreference();        
+                firefoxDriver = new FirefoxDriver(optionsFirefox);
+
+                //Firefox stary
+                //FirefoxOptions options = new FirefoxOptions();
+                //options.UseLegacyImplementation = true;
+                //driver = new FirefoxDriver(options);
+            }
+
+            if (nazwaPrzegladarki == "IE")
+            {
+                //W przeciwieństwie do innych języków, w C# konstruktor sterownika zazwyczaj przyjmuje nie obiekt typu ICapabilities,
+                //a bardziej specjalistyczny obiekt, który zawiera zestaw opcji sterownika, i dla każdego sterownika jest swój. 
+                InternetExplorerOptions optionsIE = new InternetExplorerOptions();
+                optionsIE.UnexpectedAlertBehavior = InternetExplorerUnexpectedAlertBehavior.Dismiss;
+                optionsIE.RequireWindowFocus = true;
+                optionsIE.IgnoreZoomLevel = true;
+                //lepiej samemu ustawić ręcznie w przegladarce tryb chroniony, ustawienie poniżej czasami działa żle 
+                optionsIE.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+                ieDriver = new InternetExplorerDriver(optionsIE);
+            }
+
+            if (nazwaPrzegladarki == "Chrome")
+            {
+                ChromeOptions optionsChrome = new ChromeOptions();
+                //wskazanie ścieżki do pliku
+                //optionsChrome.BinaryLocation = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+                //optionsChrome.AddArguments("start-fullscreen");
+                optionsChrome.AddArguments("start-maximized");
+                //instalacja rozszerzenia
+                //optionsChrome.AddExtensions(@"C:\Program Files (x86)\Google\Chrome\Application\extension.crx");
+                Console.WriteLine(optionsChrome.ToCapabilities());
+                chromeDriver = new ChromeDriver(optionsChrome);
+            }
+                 
+            wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
         public void FirstTest()
         {
-            driver.Url = "http://www.google.pl";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver");
-            driver.FindElement(By.Name("btnG")).Click();
-            wait.Until(ExpectedConditions.TitleIs("webdriver - Szukaj w Google"));  
+            chromeDriver.Url = "http://www.google.pl";
+            chromeDriver.FindElement(By.Name("q")).SendKeys("webdriver");
+            chromeDriver.FindElement(By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - Szukaj w Google"));
+
+            //cookies
+            //Przyłączanie do istniejacej sesji
+            chromeDriver.Manage().Cookies.AddCookie(new Cookie("test", "test"));
+            //Cookies według określonej nazwy
+            Cookie testCookie = chromeDriver.Manage().Cookies.GetCookieNamed("test");
+            Console.WriteLine(testCookie);
+            //Lista cookies
+            ICollection<Cookie> cookies = chromeDriver.Manage().Cookies.AllCookies;
+            foreach (Cookie link in cookies)
+            {
+                Console.WriteLine(link);
+            }
+
+            chromeDriver.Manage().Cookies.DeleteCookieNamed("test");
+            chromeDriver.Manage().Cookies.DeleteAllCookies();
         }
 
         [TearDown]
         public void stop()
         {
-            driver.Quit();
-            driver = null;
+            chromeDriver.Quit();
+            chromeDriver = null;
         }
       }
     }
